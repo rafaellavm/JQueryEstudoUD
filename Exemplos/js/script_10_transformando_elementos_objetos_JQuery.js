7552c03a2ce1aa52766c1a73621613e5a8aa57eb
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-12-14 13:16:18 -0200

    (02)(10) Transformando elementos em objetos JQuery

--- Exemplos/js/script_10_transformando_elementos_objetos_JQuery.js
+++ Exemplos/js/script_10_transformando_elementos_objetos_JQuery.js
@@ -9,6 +9,18 @@ $(function () {
 
     var div2 = $('#div');
 
-    console.log(div2);
+    //console.log(div2);
+
+    //02) Numa lista, selecionar o segundo item dele:
+
+    //coloca eq(1) porque começa do 0
+    //console.log($('li:eq(1)'));
+
+    //ou então selecionar todos os elementos e depois selecionar só elemento
+    //console.log($('li').eq(1));
+
+    var segundoItem = $('li').eq(1);
+    segundoItem.html('Segundo item');
+
 
 });
