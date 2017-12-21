af1892484653b9d5dfe56cc0596954a879ba6460
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-12-21 14:00:46 -0200

    (02) (16) Como saber se um elemento tem uma classe em particular

---
+++ Exemplos/js/script_16_como_saber_elemento_tem_classe_em_particular.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    //01) quero verificar se meu input tem a classe 'inputtext' nele (tem)
+
+    if($('input').hasClass('inputtext')){
+        console.log('o input tem a classe!');
+    }else{
+        console.log('o input não tem a classe!');
+    }
+
+
+    //------------------------------------------------------------------------------------------
+
+
+
+
+});
