0eee6b3910b0b2c9ee7c7b45856d38d1985a0faf
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-12-21 13:49:34 -0200

    (02) (15) Como saber se um elemento existe

---
+++ Exemplos/js/script_15_como_saber_elemento_existe.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    //01) se eu quiser saber se existe uma div com id 'algo' (não existe)
+
+    if ($('#algo').length > 0) { //vale 0 porque não existe
+        console.log(' div Existe!')
+    } else {
+        console.log('div Não existe!!')
+    }
+
+
+    //------------------------------------------------------------------------------------------
+
+
+
+
+});
